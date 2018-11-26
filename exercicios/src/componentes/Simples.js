d2e29ea194923193138577088d4f9f928854debd
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2018-11-26 21:56:29 -0200

    Criando estilo

--- exercicios/src/componentes/Simples.js
+++ exercicios/src/componentes/Simples.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import { Text } from 'react-native'
+import Padrao from '../estilo/Padrao'
 
 // export default function(props) {
 //     return <Text>{props.texto}</Text>
 // }
 
-export default props => [
-    <Text key={1}>Arrow 1: {props.texto}</Text>,
-    <Text key={2}>Arrow 2: {props.texto}</Text>
-]
+export default props => 
+    <Text style={Padrao.ex} >Arrow 1: {props.texto}</Text>
     
